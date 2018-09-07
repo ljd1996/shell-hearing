dac40564351ab95381c212fbe26c6ba8946aa7d5
hearing <1124031072@qq.com>
2018-09-07 10:04:21 +0800

    first commit

---
+++ sublime.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# SUB3_HOME=/snap/bin/
+CMD="LD_PRELOAD=/home/hearing/WorkSpace/shell/libsublime-imfix.so sublime-text.subl"
+# FILENAME=$1
+# if [ -n "$1" ]
+# then
+# CMD=${CMD}" "`pwd`/$FILENAME
+# fi
+# cd "$SUB3_HOME"
+eval $CMD
